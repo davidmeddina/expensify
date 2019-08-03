a1ec36b25ba702f295b84407ca6d224f6ea9984d
david medina <davidmeddina@gmail.com>
2019-08-03 16:43:46 -0500

    Categories CRUD

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,23 +1,47 @@
+# frozen_string_literal: true
+
 class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[show update edit destroy]
 
-def new
-  @category = Category.new
-end
-
-def create
-  @category = Category.new(category_params)
-  if @category.save
-    redirect_to expenses_path
-    flash[:notice] = 'Categoria creada'
-  else
-    render :new
+  def index
+    @categories = Category.all
   end
-end
 
-private
+  def new
+    @category = Category.new
+  end
 
-def category_params
-  params.require(:category).permit(:name)
-end
+  def edit; end
 
+  def update
+    if @category.update(category_params)
+      redirect_to categories_path
+    else
+      render :edit
+    end
+  end
+
+  def create
+    if @category.save
+      redirect_to categories_path
+      flash[:notice] = 'Categoria creada'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_path
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
