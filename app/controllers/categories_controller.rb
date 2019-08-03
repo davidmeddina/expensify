ce2cce161966b97b062aef939926af4b71a56dcd
david medina <davidmeddina@gmail.com>
2019-08-03 14:49:16 -0500

    Category create code

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,23 @@
+class CategoriesController < ApplicationController
+
+def new
+  @category = Category.new
+end
+
+def create
+  @category = Category.new(category_params)
+  if @category.save
+    redirect_to expenses_path
+    flash[:notice] = 'Categoria creada'
+  else
+    render :new
+  end
+end
+
+private
+
+def category_params
+  params.require(:category).permit(:name)
+end
+
+end
